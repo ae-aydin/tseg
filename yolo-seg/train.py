5826bc0b6b36853654dd678e54cbc4e5950938ac
Arif Enes Aydın <aydnarifenes@gmail.com>
2024-04-08 22:48:29 +0300

    Updated train.py

--- yolo-seg/train.py
+++ yolo-seg/train.py
@@ -3,18 +3,17 @@ from ultralytics import YOLO
 model = YOLO("yolov8m-seg.pt")
 
 if __name__ == "__main__":
-    train_results = model.train(
+    model.train(
         data="data.yaml",
         batch=16,
-        epochs=300,
+        epochs=600,
         imgsz=640,
         single_cls=True,
-        patience=50,
+        overlap_mask=False,
+        patience=300,
         cache=True,
-        plots=True,
+        plots=True
     )
 
-    val_results = model.val(data="data.yaml", imgsz=640, batch=16, conf=0.25, iou=0.6)
-
-    model.export(format="onnx")
-
+    model.val(data="data.yaml", imgsz=640, batch=16, plots=True)
+    

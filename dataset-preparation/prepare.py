669c35a87ba99ed8456141b61b9f82f3515127fc
ae-aydin <aydnarifenes@gmail.com>
2025-05-15 00:49:28 +0300

    Restructure the dataset preparation - made YOLO format optional, default is common comp vision dataset format usage

--- dataset-preparation/prepare.py
+++ dataset-preparation/prepare.py
@@ -3,18 +3,28 @@ from pathlib import Path
 import typer
 from loguru import logger
 
-from tseg.split import train_test_split
+from tseg import train_test_split
 
 
 def main(
     tiles_path: str,  # path containing tile folders
     export_path: str,  # path to create dataset folder
     tile_count: int = -1,  # maximum tile count in a wsi
-    ratio: float = 0.7,  # train-test split
+    train_ratio: float = 0.6,
+    val_ratio: float = 0.2,
+    yolo_format: bool = False,  # create yolo dataset
     visualize: bool = False,  # visualize yolo annotations
 ):
     logger.info("Dataset Preparation for tseg")
-    train_test_split(Path(tiles_path), Path(export_path), tile_count, ratio, visualize)
+    train_test_split(
+        Path(tiles_path),
+        Path(export_path),
+        tile_count,
+        train_ratio,
+        val_ratio,
+        yolo_format,
+        visualize,
+    )
     logger.info("Finished.")
 
 
--- dataset-preparation/tseg/split.py
+++ dataset-preparation/tseg/split.py
@@ -5,7 +5,8 @@ from pathlib import Path
 from loguru import logger
 from tqdm import tqdm
 
-from tseg.yolo_formatter import convert_to_yolo_format
+from tseg.convert_yolo import convert_to_yolo_format
+from tseg.dataset_yolo import prepare_yolo_dataset
 
 DATA_CATEGORIES = ["wsi_tiled", "img_tiled"]
 
@@ -70,8 +71,8 @@ def _copy_tiles(
     Args:
         tiles_path (Path): Path containing all tile folders.
         subset_tile_folders (list): Train or test tile folder list.
-        subset_images_path (Path): train/images or test/images path.
-        subset_masks_path (Path): train/masks or test/masks path.
+        subset_images_path (Path): train/images, val/images, or test/images path.
+        subset_masks_path (Path): train/masks, val/masks, or test/masks path.
         tile_count (int): Maximum number of tiles to copy.
     """
     for folder in tqdm(
@@ -83,7 +84,7 @@ def _copy_tiles(
         source_masks = tiles_path / folder / "masks"
 
         source_images_list = list(source_images.iterdir())
-        
+
         # Apply tile count limit only to "wsi_tiled" folders if specified
         if tile_count != -1 and source_images.parent.name.split("|")[0] == "wsi_tiled":
             tiles_per_wsi = min(tile_count, len(source_images_list))
@@ -104,6 +105,7 @@ def _accumulate_tiles(
     tiles_path: Path,
     export_path: Path,
     train_tile_folders: list,
+    val_tile_folders: list,
     test_tile_folders: list,
     tile_count: int,
 ):
@@ -114,12 +116,15 @@ def _accumulate_tiles(
         tiles_path (Path): Path containing all tile folders.
         export_path (Path): Path to export (dataset) folder.
         train_tile_folders (list): Tile folder names selected for train set.
+        val_tile_folders (list): Tile folder names selected for validation set.
         test_tile_folders (list): Tile folder names selected for test set.
         tile_count (int): Maximum tile count in a wsi.
     """
     # Define dataset folder structure
     train_images_path = export_path / "train" / "images"
     train_masks_path = export_path / "train" / "masks"
+    val_images_path = export_path / "val" / "images"
+    val_masks_path = export_path / "val" / "masks"
     test_images_path = export_path / "test" / "images"
     test_masks_path = export_path / "test" / "masks"
 
@@ -127,6 +132,8 @@ def _accumulate_tiles(
     for path in [
         train_images_path,
         train_masks_path,
+        val_images_path,
+        val_masks_path,
         test_images_path,
         test_masks_path,
     ]:
@@ -138,69 +145,20 @@ def _accumulate_tiles(
         tiles_path, train_tile_folders, train_images_path, train_masks_path, tile_count
     )
     _copy_tiles(
+        tiles_path, val_tile_folders, val_images_path, val_masks_path, tile_count
+    )
+    _copy_tiles(
         tiles_path, test_tile_folders, test_images_path, test_masks_path, tile_count
     )
-
-
-def _prepare_yolo_dataset(export_path: Path):
-    """
-    Prepare the YOLO dataset according to the format.
-
-    Args:
-        export_path (Path): Path to export (dataset) folder.
-    """
-    yolo_dataset_path = export_path / "yolo-dataset"
-    train_path = export_path / "train"
-    test_path = export_path / "test"
-
-    yolo_dataset_path.mkdir(parents=True, exist_ok=True)
-
-    img_train = yolo_dataset_path / "images" / "train"
-    img_val = yolo_dataset_path / "images" / "val"
-    label_train = yolo_dataset_path / "labels" / "train"
-    label_val = yolo_dataset_path / "labels" / "val"
-
-    img_train.mkdir(parents=True, exist_ok=True)
-    img_val.mkdir(parents=True, exist_ok=True)
-    label_train.mkdir(parents=True, exist_ok=True)
-    label_val.mkdir(parents=True, exist_ok=True)
-
-    logger.info("Arranging dataset in YOLO format.")
-
-    for file in tqdm(
-        list((train_path / "images").iterdir()),
-        desc=f"Copying files to {img_train}",
-        ncols=150,
-    ):
-        shutil.copy(file, img_train)
-
-    for file in tqdm(
-        list((test_path / "images").iterdir()),
-        desc=f"Copying files to {img_val}",
-        ncols=150,
-    ):
-        shutil.copy(file, img_val)
-
-    for file in tqdm(
-        list((train_path / "annotations").iterdir()),
-        desc=f"Copying files to {label_train}",
-        ncols=150,
-    ):
-        shutil.copy(file, label_train)
-
-    for file in tqdm(
-        list((test_path / "annotations").iterdir()),
-        desc=f"Copying files to {label_val}",
-        ncols=150,
-    ):
-        shutil.copy(file, label_val)
 
 
 def train_test_split(
     tiles_path: Path,
     export_path: Path,
     tile_count: int,
-    ratio: float,
+    train_ratio: float,
+    val_ratio: float,
+    yolo_format: bool,
     visualize: bool,
 ):
     """
@@ -210,25 +168,42 @@ def train_test_split(
         tiles_path (Path): Path containing all tile folders.
         export_path (Path): Path where dataset folder will be created.
         tile_count (float): Maximum tile count in a wsi.
-        ratio (float): Train-test split ratio.
+        train_ratio (float): Train set ratio.
+        val_ratio (float): Validation set ratio.
+        yolo_format (bool): Create YOLO format.
         visualize (bool): Whether to visualize YOLO format to check consistency.
     """
+    # If sum of train ratio and test ratio close to 1, dont use test set
+    create_test_set = False if np.isclose(train_ratio + val_ratio, 1) else True
     tile_folders_categorized = _count_data(tiles_path)
-    train_tile_folders, test_tile_folders = [], []
+    train_tiles, val_tiles, test_tiles = [], [], []
     for tile_folders in tile_folders_categorized.values():
-        temp_n_train = int(round(len(tile_folders) * ratio))
+        n_train = int(round(len(tile_folders) * train_ratio))
+        n_val = int(round(len(tile_folders) * val_ratio))
         np.random.shuffle(tile_folders)
-        train_tile_folders.extend(tile_folders[:temp_n_train])
-        test_tile_folders.extend(tile_folders[temp_n_train:])
+        train_tiles.extend(tile_folders[:n_train])
+        if create_test_set:
+            val_tiles.extend(tile_folders[n_train : n_train + n_val])
+            test_tiles.extend(tile_folders[n_train + n_val :])
+        else:
+            val_tiles.extend(tile_folders[n_train:])
+
     logger.info(
-        f"Train tiles ({len(train_tile_folders)} total)\n\t{'\n\t'.join(map(lambda x: str(x.name), train_tile_folders))}"
+        f"Train tiles ({len(train_tiles)} total)\n\t{'\n\t'.join(map(lambda x: str(x.name), train_tiles))}"
     )
     logger.info(
-        f"Test tiles ({len(test_tile_folders)} total)\n\t{'\n\t'.join(map(lambda x: str(x.name), test_tile_folders))}"
+        f"Validation tiles ({len(val_tiles)} total)\n\t{'\n\t'.join(map(lambda x: str(x.name), val_tiles))}"
     )
+    logger.info(
+        f"Test tiles ({len(test_tiles)} total)\n\t{'\n\t'.join(map(lambda x: str(x.name), test_tiles))}"
+    )
+
     _accumulate_tiles(
-        tiles_path, export_path, train_tile_folders, test_tile_folders, tile_count
+        tiles_path, export_path, train_tiles, val_tiles, test_tiles, tile_count
     )
-    convert_to_yolo_format(export_path / "train", visualize)
-    convert_to_yolo_format(export_path / "test", visualize)
-    _prepare_yolo_dataset(export_path)
+
+    if yolo_format:
+        convert_to_yolo_format(export_path / "train", visualize)
+        convert_to_yolo_format(export_path / "val", visualize)
+        convert_to_yolo_format(export_path / "test", visualize)
+        prepare_yolo_dataset(export_path)

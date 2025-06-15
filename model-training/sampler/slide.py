70010825a3fe00141b602800e9eca9745c9f0046
ae-aydin <aydnarifenes@gmail.com>
2025-06-16 02:45:11 +0300

    Add seed for reproducibility

--- model-training/sampler/slide.py
+++ model-training/sampler/slide.py
@@ -6,10 +6,11 @@ from torch.utils.data import Sampler
 
 
 class SlideBalancedSampler(Sampler):
-    def __init__(self, dataset, samples_per_epoch: int):
+    def __init__(self, dataset, samples_per_epoch: int, seed: int = 42):
         super().__init__(dataset)
         self.dataset = dataset
         self.samples_per_epoch = samples_per_epoch
+        self.seed = seed
 
         self.slide_to_indices = defaultdict(list)
         for idx, slide_name in enumerate(self.dataset.df["slide_name"]):
@@ -18,13 +19,17 @@ class SlideBalancedSampler(Sampler):
         self.slide_ids = list(self.slide_to_indices.keys())
 
     def __iter__(self):
-        slide_deck = np.random.choice(
+        # Use seeded random generators
+        rng = np.random.default_rng(self.seed)
+        random_rng = random.Random(self.seed)
+        
+        slide_deck = rng.choice(
             self.slide_ids, self.samples_per_epoch, replace=True
         )
         epoch_indices = []
         for slide_id in slide_deck:
             possible_indices = self.slide_to_indices[slide_id]
-            chosen_index = random.choice(possible_indices)
+            chosen_index = random_rng.choice(possible_indices)
             epoch_indices.append(chosen_index)
         return iter(epoch_indices)
 
